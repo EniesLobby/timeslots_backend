b21ce21227480b4b9f9bd6d528b706a5f1856da4
Ruslan Fazliev <ruslan1fazliev@gmail.com>
2018-08-18 18:42:41 +0200

    added validations

--- timeslots_app/serializers.py
+++ timeslots_app/serializers.py
@@ -12,12 +12,45 @@ class CandidateSerializer(serializers.ModelSerializer):
         fields = ('id', 'first_name', 'last_name', 'email')
 
 class CandidateTimeSlotSerializer(serializers.ModelSerializer):
+    def validate(self, data):
+        errors = {}
+
+        if ('start_date' in data and 'end_date' in data):
+            start_date = data['start_date']
+            end_date = data['end_date'] 
+
+            if(start_date > end_date):
+                errors['error'] = u'end date before start date error'
+            
+            if(start_date == end_date):
+                errors['error'] = u'end date should not be equal to start date'
+            raise serializers.ValidationError(errors)
+
+        return data
+
     class Meta:
         model = TimeSlot_Candidate  
         fields = ('id', 'candidate', 'start_date', 'end_date')
 
 
 class InterviewerTimeSlotSerializer(serializers.ModelSerializer):
+
+    def validate(self, data):
+        errors = {}
+
+        if ('start_date' in data and 'end_date' in data):
+            start_date = data['start_date']
+            end_date = data['end_date'] 
+
+            if(start_date > end_date):
+                errors['error'] = u'end date before start date error'
+            
+            if(start_date == end_date):
+                errors['error'] = u'end date should not be equal to start date'
+            raise serializers.ValidationError(errors)
+            
+        return data
+
     class Meta:
         model = TimeSlot_Interviewer
         fields = ('id', 'interviewer', 'start_date', 'end_date')
